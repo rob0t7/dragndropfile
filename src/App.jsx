280ca6e6e7c4ab5afdffddd6151889afdaba52dd
Robert Jackiewicz <rob@jackiewicz.ca>
2024-02-21 08:55:43 -0500

    Starting point

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { Button } from "./Button";
+import { FileDropzone } from "./FileDropzone";
+import { InputField } from "./InputField";
+
 function App() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    const fields = Object.fromEntries(form);
+    console.log("fields: ", fields);
+  }
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      <div className="mx-auto max-w-7x1 sm:px-6 lg:px-8">
+        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+          Drag & Drop Demo
+        </h1>
+        <form onSubmit={handleSubmit}>
+          <div className="grid gap-6 mb-6">
+            <InputField label="Name" name="name" placeholder="Full name" />
+            <InputField label="Email" name="email" placeholder="Email" />
+            <FileDropzone />
+            <Button type="submit">Submit</Button>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
---
+++ src/InputField.jsx
@@ -0,0 +1,21 @@
+export function InputField({ label, name, placeholder }) {
+  return (
+    <div>
+      <label
+        htmlFor={name}
+        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+      >
+        {label}
+      </label>
+      <div className="mt-2">
+        <input
+          type="text"
+          name={name}
+          id={name}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder={placeholder}
+        />
+      </div>
+    </div>
+  );
+}
